250824c812d00c67512dbd576b927935a9aec38d
Oguzhan Demir <oguzhandemir92@gmail.com>
2015-03-05 00:59:25 +0200

    Autocomplete is added. Some other fixes.

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ["kendo.directives"]);
 
 myApp.service('trendingService', function ($http) {
     var baseUrl = 'https://githubtrendingbe-ozi3.c9.io/';
