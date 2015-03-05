ef629d1a8f6533b466c1c68596c5e069ff1bf7b7
Oguzhan Demir <oguzhandemir92@gmail.com>
2015-03-05 14:09:40 +0200

    autocomplete done fixed popup size done.

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['autocomplete']);
 
 myApp.service('trendingService', function ($http) {
     var baseUrl = 'https://githubtrendingbe-ozi3.c9.io/';
