a9acfb58ddb2271c0bd8fdcb2c1b1591ef24e1c6
Oguzhan Demir <oguzhandemir92@gmail.com>
2015-03-05 17:46:59 +0200

    image update.

---
+++ localhost_files/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angNewsApp
+ * @description
+ * # angNewsApp
+ *
+ * Main module of the application.
+ */
+//
+var app = angular
+    .module('collaborativeEditor', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'textAngular',
+        'btford.socket-io'
+    ]);
+
